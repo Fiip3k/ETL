cbe4658cbf113e67d6d2dadb305d61db49a15ff0
Fiip3k <fikszyn3k@gmail.com>
2023-06-19 15:11:13 +0200

    memory cleanup, wait for tasks to finish b4 exit

---
+++ main.py
@@ -0,0 +1,37 @@
+#!source venv/bin/activate
+#!call venv/bin/activate
+
+import time
+from newDataHandler import NewDataHandler
+
+
+def main():
+    ndh = NewDataHandler()
+    thread, exitEvent = ndh.msgr.startListening(
+        "wgcpspem-test", handler=ndh.startMessageProcessor
+    )
+    print("Listener started...")
+    ndh.periodicCleanUp(10)
+
+    time.sleep(3)
+
+    topic = "wgcpspem-test"
+    filename = "test.csv"
+    ndh.sendUploadMessage(topic, filename, "newdata/test", overwrite=False)
+
+    time.sleep(3)
+
+    ndh.sendDownloadMessage(topic, "newdata/test", filename, "downloadedTest.csv")
+    print("Messages sent...")
+
+    input()
+    exitEvent.set()
+
+    thread.join()
+    ndh.join()
+    ndh.stopPeriodicCleanUp()
+    print("Listener finished.")
+
+
+if __name__ == "__main__":
+    main()
--- newDataHandler.py
+++ newDataHandler.py
@@ -1,5 +1,3 @@
-#!source venv/bin/activate
-
 import time
 from messenger import Messenger
 from lakeClient import LakeClient
@@ -8,46 +6,87 @@ from threading import Thread
 
 
 class NewDataHandler:
-
     lakeClient = None
+    msgr = None
+    threads = []
+    cleanup = False
+    period = 10
 
     def __init__(self) -> None:
         self.lakeClient = LakeClient("fiip3kdatalake")
+        self.msgr = Messenger()
 
-    def messageReceived(self, msg: dict):
+    def periodicCleanUp(self, period: float = 10):
+        def cleanThreads():
+            toRemove = []
+            for thread in self.threads:
+                if not thread.is_alive():
+                    toRemove.append(thread)
+            for thread in toRemove:
+                self.threads.remove(thread)
+
+            if self.cleanup:
+                time.sleep(self.period)
+                cleanThreads()
+
+        self.period = period
+        self.cleanup = True
+        Thread(target=cleanThreads).start()
+
+    def stopPeriodicCleanUp(self):
+        self.cleanup = False
+
+    def startMessageProcessor(self, msg: dict):
         def processMessage():
-            if(msg["message"]=="upload"):
-                result = self.lakeClient.uploadFile(msg['filename'], msg["path"], overwrite=msg["overwrite"])
-            elif(msg["message"]=="download"):
-                result = self.lakeClient.downloadFile(msg['path'], msg['filename'], msg['saveas'])
+            if msg["message"] == "upload":
+                result = self.lakeClient.uploadFile(
+                    msg["filename"], msg["path"], overwrite=msg["overwrite"]
+                )
+            elif msg["message"] == "download":
+                result = self.lakeClient.downloadFile(
+                    msg["path"], msg["filename"], msg["saveas"]
+                )
             else:
                 print(msg)
-                time.sleep(10)
 
         thread = Thread(target=processMessage, daemon=False)
         thread.start()
+        self.threads.append(thread)
 
-def main():
-    ndh = NewDataHandler()
-    msgr = Messenger()
-    thread, exitEvent = msgr.startListening("wgcpspem-test", handler=ndh.messageReceived, daemon = None)
-    print("Listener started...")
-    
-    time.sleep(5)
-    current_datetime = datetime.now()
-    formatted_datetime = current_datetime.strftime("%H:%M:%S %d.%m.%Y")
+    def sendUploadMessage(
+        self, topic: str, filename: str, path: str = "", overwrite: bool = True
+    ):
+        current_datetime = datetime.now()
+        formatted_datetime = current_datetime.strftime("%H:%M:%S %d.%m.%Y")
+        self.msgr.sendMessage(
+            topic,
+            {
+                "message": "upload",
+                "path": path,
+                "filename": filename,
+                "overwrite": overwrite,
+                "datetime": formatted_datetime,
+            },
+        )
 
-    filename = "test.csv"
+    def sendDownloadMessage(
+        self, topic: str, path: str, filename: str, saveas: str = None
+    ):
+        if not saveas:
+            saveas = filename
+        current_datetime = datetime.now()
+        formatted_datetime = current_datetime.strftime("%H:%M:%S %d.%m.%Y")
+        self.msgr.sendMessage(
+            topic,
+            {
+                "message": "download",
+                "path": path,
+                "filename": filename,
+                "saveas": saveas,
+                "datetime": formatted_datetime,
+            },
+        )
 
-    msgr.sendMessage("wgcpspem-test", {"message":"upload", "path":"newdata/test", "filename":filename, "overwrite":True, "datetime":formatted_datetime})
-    time.sleep(5)
-    msgr.sendMessage("wgcpspem-test", {"message":"download", "path":"newdata/test", "filename":filename, "saveas":"downloadedTest.csv", "datetime":formatted_datetime})
-    print("Messages sent...")
-
-    input()
-    exitEvent.set()
-    thread.join()
-    print("Listener finished.")
-
-if __name__ == "__main__":
-    main()
+    def join(self):
+        for thread in self.threads:
+            thread.join()
